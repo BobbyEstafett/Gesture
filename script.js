21a2909ec27c67960287e596e51b52d5f65fbfc3
Bobby Estafett <133015429+BobbyEstafett@users.noreply.github.com>
2026-03-20 15:27:23 +0100

    add files

---
+++ script.js
@@ -0,0 +1,3 @@
+body { margin: 0; overflow: hidden; background: #000; }
+.output_canvas { width: 100vw; height: 100vh; display: block; }
+.input_video { display: none; } /* On cache la vidéo brute *///
